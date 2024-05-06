49dc88a0eaf663c02fd4587b7c438dd677179340
marco_dev <87571651+dynamicadev@users.noreply.github.com>
2024-05-06 18:14:24 -0500

    otra

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -3,14 +3,7 @@
 import React from 'react'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+import { Sheet,SheetContent,SheetTrigger } from "@/components/ui/sheet"
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, UserButton } from '@clerk/nextjs'
