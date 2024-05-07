27db8b422e6fb1b3e54e0d4362d3749b864838d9
marco_dev <87571651+dynamicadev@users.noreply.github.com>
2024-05-06 22:01:48 -0500

    clerk webhooks 3

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -65,12 +65,12 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
-    const newUser = await createUser(user); 
+    const newUser = await createUser(user);  
 
     // Set public metadata
     if (newUser) {
@@ -89,8 +89,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
       photo: image_url,
     };
